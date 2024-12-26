98f791e6c6b0a089054f6cb4a918f3b5dfc1c9b8
Keshav Prajapati <keshavpj1711@gmail.com>
2024-12-26 10:42:11 +0530

    basic recursion done

--- 0Basics/Recursion/basic.cpp
+++ 0Basics/Recursion/basic.cpp
@@ -12,10 +12,43 @@ void printNTimes(int n, string& str){
 }
 
 void Solve(){
+  // Prints a string N times
   string anyStr;
   int n;
   cin>>anyStr>>n;
   printNTimes(n, anyStr);
+}
+
+void Solve1(int n){
+  // Prints N to 1
+  if (n==1) {
+    cout<<1;
+    return;
+  } else {
+    cout<<n;
+    Solve1(n-1);
+  }
+}
+
+void Solve2(int i, int n){
+  // Prints 1 to N
+  if (i==n) {
+    cout<<n;
+    return;
+  } else {
+    cout<<i;
+    Solve2(i+1, n);
+  }
+}
+
+int SolveSum(int n){
+  // Printing sum of N terms
+  if (n==0) {
+   return 0;
+  }
+  int sum = n;
+  sum += SolveSum(n-1);
+  return sum;
 }
 
 int main()
@@ -23,7 +56,10 @@ int main()
   ios_base::sync_with_stdio(0);
   cin.tie(0);cout.tie(0);
 
-  Solve();
+  // Solve();
+  // Solve1(5);
+  // Solve2(1, 5);
+  cout<<SolveSum(99);
 
   return 0;
 }
