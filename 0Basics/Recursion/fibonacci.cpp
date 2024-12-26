73a35858b6e616b14f5ebaa3fa6d0d6f85212faa
Keshav Prajapati <keshavpj1711@gmail.com>
2024-12-26 13:22:15 +0530

    fibonacci

---
+++ 0Basics/Recursion/fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Solve(int n){
+  if (n<=2) {
+    return 1;
+  }
+  return Solve(n-1)+Solve(n-2);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);cout.tie(0);
+
+  int n;
+  cin>>n;
+  for (int i=1; i<=n; i++) {
+    cout<<Solve(i)<<" ";
+  }
+
+  return 0;
+}
